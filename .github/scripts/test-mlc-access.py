255341927d395ec78e80d778ee0af308fff99424
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-02-15 06:31:07 +0530

    Update test-mlc-access.py

--- .github/scripts/test-mlc-access.py
+++ .github/scripts/test-mlc-access.py
@@ -2,153 +2,441 @@ import os
 import subprocess
 import mlc
 
+def process_output(target, action, res):
+    if action in ["find"]:
+        if "list" not in res:
+            raise Exception("'list' entry not found in find result")
+            return  # Exit function if there's an error
+        if len(res['list']) == 0:
+            print(f""" WARNING: No entry found for the particular action and target!""")
+        else:
+            for item in res['list']:
+                print(f"""Item path: {item.path}""")
+                if action == "show":
+                    print(f"{target} meta:")
+                    print(item.meta)
 
-###### TEST 1 - pull repo - Pull a forked MLOps repository
-print("###### TEST 1 - pull repo - Pull a forked MLOps repository")
-# Define paths
-GH_MLC_REPO_PATH_FORK = os.path.join(os.path.expanduser("~"), "MLC", "repos", "anandhu-eng@mlperf-automations")
-GH_MLC_REPO_JSON_PATH = os.path.join(os.path.expanduser("~"), "MLC", "repos", "repos.json")
+def test_find_repo():
+    # This function is seperately written to include the actions which is needed to be tested in a forked repository
+    ###### TEST - find repo
 
-res = mlc.access({
-    "automation": "repo",
-    "action": "pull",
-    "repo": "anandhu-eng@mlperf-automations",
-    "checkout": "dev"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+    print("###### TEST - find repo")
+    # format: <repo_owner>@<repos_name>
+    res = mlc.access({
+        "automation": "repo",
+        "action": "find",
+        "repo": "anandhu-eng@mlperf-automations"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find repo) - <repo_owner>@<repos_name>")
+        process_output("repo", "find", res)
 
-# Check if the repository folder exists
-if not os.path.isdir(GH_MLC_REPO_PATH_FORK):
-    raise Exception(f"Repository folder {GH_MLC_REPO_PATH_FORK} not found. Exiting with failure.")
+    # format: <repo_url>
+    res = mlc.access({
+        "automation": "repo",
+        "action": "find",
+        "repo": "https://github.com/mlcommons/mlperf-automations.git"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find repo) - <repo_url>")
+        process_output("repo", "find", res)
+
+    # format: <repo_uid>
+    res = mlc.access({
+        "automation": "repo",
+        "action": "find",
+        "repo": "9cf241afa6074c89"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find repo) - <repo_uid>")
+        process_output("repo", "find", res)
+
+    # format: <repo_alias>
+    res = mlc.access({
+        "automation": "repo",
+        "action": "find",
+        "repo": "mlcommons@mlperf-automations"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find repo) - <repo_alias>")
+        process_output("repo", "find", res)
+
+    # format: <repo_alias>,<repo_uid>
+    res = mlc.access({
+        "automation": "repo",
+        "action": "find",
+        "repo": "mlcommons@mlperf-automations,9cf241afa6074c89"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find repo) - <repo_alias>,<repo_uid>")
+        process_output("repo", "find", res)
+
+    print("###### TEST find repo SUCCESSFUL.")
+
+def test_list_repo():
+    print("###### TEST - list repo")
+    res = mlc.access({
+        "automation": "repo",
+        "action": "list"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
     
-# Check if the JSON file exists
-if not os.path.isfile(GH_MLC_REPO_JSON_PATH):
-    raise Exception(f"File {GH_MLC_REPO_JSON_PATH} does not exist. Exiting with failure.")
+    print("###### TEST list repo SUCCESSFUL.")
 
-# Check if the repository path is in the JSON file
-with open(GH_MLC_REPO_JSON_PATH, 'r') as json_file:
-    if GH_MLC_REPO_PATH_FORK not in json_file.read():
-        raise Exception(f"Path {GH_MLC_REPO_PATH_FORK} not found in {GH_MLC_REPO_JSON_PATH}. Exiting with failure.")
+def test_find_cache():
+    print("###### TEST - find cache")
+    res = mlc.access({
+        "automation": "cache",
+        "action": "find",
+        "tags": "get,imagenet-aux"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find cache)")
+        process_output("cache", "find" ,res)
+    
+    print("###### TEST find cache SUCCESSFUL.")
 
-# Check the current branch
-try:
-    CURRENT_BRANCH = subprocess.check_output(
-        ["git", "-C", GH_MLC_REPO_PATH_FORK, "rev-parse", "--abbrev-ref", "HEAD"],
-        text=True
-    ).strip()
-except subprocess.CalledProcessError:
-    raise Exception(f"Failed to get the current branch for {GH_MLC_REPO_PATH_FORK}. Exiting with failure.")
+def test_show_cache():
+    print("###### TEST - show cache")
+    res = mlc.access({
+        "automation": "cache",
+        "action": "show",
+        "tags": "get,imagenet-aux"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(show cache)")
+        process_output("cache", "show" ,res)
+    
+    print("###### TEST show cache SUCCESSFUL.")
 
-if CURRENT_BRANCH != "dev":
-    raise Exception(f"Expected branch 'dev', but found '{CURRENT_BRANCH}'. Exiting with failure.")
+def test_rm_cache():
+    print("###### TEST - rm cache")
+    res = mlc.access({
+        "automation": "cache",
+        "action": "rm",
+        "tags": "get,imagenet-aux",
+        "target": "cache",
+        "f": True
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+def test_cp_script():
+    print("###### TEST - cp script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "cp",
+        "src": "detect-os",
+        "dest": "my-os-detect"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    print("###### TEST cp script SUCCESSFUL.")
 
-print("###### TEST 1 SUCCESSFULL.")
+def test_add_repo():
+    print("###### TEST - add repo")
+    res = mlc.access({
+        "automation": "repo",
+        "action": "add",
+        "repo": "my-new-repo"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print(f"Successfully added repo")
 
-###### TEST 2 - find repo
+    res = mlc.access({
+        "automation": "repo",
+        "action": "add",
+        "repo": "https://github.com/mlcommons/inference"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print(f"Successfully added repo")
+    
+    res = mlc.access({
+        "automation": "repo",
+        "action": "add",
+        "repo": "https://mygit.com/myrepo"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print(f"Successfully added repo")
+        
+    print("###### TEST add repo SUCCESSFUL.")
 
-print("###### TEST 2 - find repo")
-# format: <repo_owner>@<repos_name>
-res = mlc.access({
-    "automation": "repo",
-    "action": "find",
-    "repo": "anandhu-eng@mlperf-automations"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+def test_add_script():
+    print("###### TEST - add script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "add",
+        "item": "my-script-1",
+        "tags": "my,new-tags-1" 
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("script with alias my-script-1 successfully added")
 
-# format: <repo_url>
-res = mlc.access({
-    "automation": "repo",
-    "action": "find",
-    "repo": "https://github.com/mlcommons/mlperf-automations.git"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+    res = mlc.access({
+        "automation": "script",
+        "action": "add",
+        "item": "my-script-2",
+        "tags": "my,new-tags-2" 
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("script with alias my-script-2 successfully added")
 
-# format: <repo_uid>
-res = mlc.access({
-    "automation": "repo",
-    "action": "find",
-    "repo": "9cf241afa6074c89"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+    res = mlc.access({
+        "automation": "script",
+        "action": "add",
+        "item": "my-script-3",
+        "tags": "my,new-tags-3",
+        "template_tags": "detect,os" 
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("script with alias my-script-3 successfully added")
 
-# format: <repo_alias>
-res = mlc.access({
-    "automation": "repo",
-    "action": "find",
-    "repo": "mlcommons@mlperf-automations"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+    res = mlc.access({
+        "automation": "script",
+        "action": "add",
+        "item": "mlcommons@mlperf-automations:my-script-4",
+        "tags": "my,new-tags-4",
+        "template_tags": "detect,os"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("script with alias my-script-4 successfully added")
 
-# format: <repo_alias>,<repo_uid>
-res = mlc.access({
-    "automation": "repo",
-    "action": "find",
-    "repo": "mlcommons@mlperf-automations,9cf241afa6074c89"
-})
-print(res)
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+    print("###### TEST add script SUCCESSFUL.")
 
-print("###### TEST 2 SUCCESSFUL.")
+def test_mv_script():
+    res = mlc.access({
+        "automation": "script",
+        "action": "mv",
+        "src": "my-script-1",
+        "dest": "moved-my-script-1"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "mv",
+        "src": "my-script-2",
+        "dest": "mlcommons@mlperf-automations:moved-my-script-2"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    print("###### TEST mv script SUCCESSFUL.")
 
-###### TEST 3 - pull repo - Test conflicting repo scenario
+def test_show_script():
+    print("###### TEST - show script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "show",
+        "tags": "run-mlperf,inference"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(show cache)")
+        process_output("script", "show" ,res)
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "show",
+        "uid": "863735b7db8c44fc"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(show cache)")
+        process_output("script", "show" ,res)
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "show",
+        "alias": "detect-os,863735b7db8c44fc"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(show cache)")
+        process_output("script", "show" ,res)
 
-print("###### TEST 3 - pull repo - Test conflicting repo scenario")
-# Define paths
-GH_MLC_REPO_PATH = os.path.join(os.path.expanduser("~"), "MLC", "repos", "mlcommons@mlperf-automations")
-GH_MLC_REPO_JSON_PATH = os.path.join(os.path.expanduser("~"), "MLC", "repos", "repos.json")
+    res = mlc.access({
+        "automation": "script",
+        "action": "show",
+        "alias": "detect-os"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(show cache)")
+        process_output("script", "show" ,res)
+    
+    print("###### TEST show script SUCCESSFUL.")
 
-# Pull the MLOps repository
-res = mlc.access({
-    "automation": "repo",
-    "action": "pull",
-    "repo": "mlcommons@mlperf-automations",
-    "checkout": "dev"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
+def test_find_script():
+    print("###### TEST - find script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "find",
+        "tags": "run-mlperf,inference"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find script)")
+        process_output("script", "find" ,res)
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "find",
+        "uid": "863735b7db8c44fc"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find script)")
+        process_output("script", "find" ,res)
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "find",
+        "alias": "detect-os,863735b7db8c44fc"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find script)")
+        process_output("script", "find" ,res)
 
-# Check if the repository folder exists
-if not os.path.isdir(GH_MLC_REPO_PATH):
-    raise Exception(f"Repository folder {GH_MLC_REPO_PATH} not found. Exiting with failure.")
+    res = mlc.access({
+        "automation": "script",
+        "action": "find",
+        "alias": "detect-os"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    else:
+        print("Output - TEST(find script)")
+        process_output("script", "find" ,res)
+    
+    print("###### TEST find script SUCCESSFUL.")
 
-# Check if the JSON file exists
-if not os.path.isfile(GH_MLC_REPO_JSON_PATH):
-    raise Exception(f"File {GH_MLC_REPO_JSON_PATH} does not exist. Exiting with failure.")
 
-# Check if the repository path is in the JSON file
-with open(GH_MLC_REPO_JSON_PATH, 'r') as json_file:
-    if GH_MLC_REPO_PATH not in json_file.read():
-        raise Exception(f"Path {GH_MLC_REPO_PATH} not found in {GH_MLC_REPO_JSON_PATH}. Exiting with failure.")
+def test_rm_script():
+    print("###### TEST - rm script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "rm",
+        "tags": "app,image,corner-detection",
+        "f": True
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "rm",
+        "item": "get-ipol-src",
+        "f": True
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "rm",
+        "item": "63080407db4d4ac4",
+        "f": True
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    print("###### TEST rm script SUCCESSFUL.")
 
-# Check for the conflicting path
-GH_MLC_REPO_PATH_FORK = os.path.join(os.path.expanduser("~"), "MLC", "repos", "anandhu-eng@mlperf-automations")
-with open(GH_MLC_REPO_JSON_PATH, 'r') as json_file:
-    if GH_MLC_REPO_PATH_FORK in json_file.read():
-        raise Exception(f"Path {GH_MLC_REPO_PATH_FORK} also found in {GH_MLC_REPO_JSON_PATH}. This should have been replaced. Exiting with failure.")
+def test_list_script():
+    print("###### TEST - list script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "list"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    print("###### TEST list script SUCCESSFUL.")
 
-# Check the current branch
-try:
-    CURRENT_BRANCH = subprocess.check_output(
-        ["git", "-C", GH_MLC_REPO_PATH, "rev-parse", "--abbrev-ref", "HEAD"],
-        text=True
-    ).strip()
-except subprocess.CalledProcessError:
-    raise Exception(f"Failed to get the current branch for {GH_MLC_REPO_PATH}. Exiting with failure.")
+def test_list_cache():
+    print("###### TEST - list cache")
+    res = mlc.access({
+        "automation": "cache",
+        "action": "list"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    print("###### TEST list cache SUCCESSFUL.")
 
-if CURRENT_BRANCH != "dev":
-    raise Exception(f"Expected branch 'dev', but found '{CURRENT_BRANCH}'. Exiting with failure.")
+def test_run_script():
+    print("###### TEST - run script")
+    res = mlc.access({
+        "automation": "script",
+        "action": "run",
+        "tags": "get,imagenet-aux"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    res = mlc.access({
+        "automation": "script",
+        "action": "run",
+        "tags": "get,imagenet-aux,_from.dropbox"
+    })
+    if res['return'] > 0:
+        raise Exception(f"{res['error']}")
+    
+    print("###### TEST run script SUCCESSFUL.")
+    
 
-res = mlc.access({
-    "automation": "repo",
-    "action": "pull"
-})
-if res['return'] > 0:
-    raise Exception(f"{res['error']}")
-
-print("###### TEST 3 SUCCESSFUL.")
+test_list_repo()
+test_find_cache()
+test_run_script()
+test_find_cache()
+test_show_cache()
+test_rm_cache()
+test_cp_script()
+test_add_repo()
+test_add_script()
+test_mv_script()
+test_show_script()
+test_find_script()
+test_rm_script()
+test_list_script()
+test_list_cache()
