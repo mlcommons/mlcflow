57cd28b01d5f9cfe8c66598de3f3cd4989beaebd
Arjun Suresh <arjun@gateoverflow.com>
2025-02-23 20:27:06 +0000

    Create action_factory.py

---
+++ mlc/action_factory.py
@@ -0,0 +1,15 @@
+from .RepoAction import RepoAction
+from .ScriptAction import ScriptAction
+from .CaacheAction import CacheAction
+
+
+# Factory to get the appropriate action class
+def get_action(target, parent):
+    action_class = actions.get(target, None)
+    return action_class(parent) if action_class else None
+
+actions = {
+        'repo': RepoAction,
+        'script': ScriptAction,
+        'cache': CacheAction
+    }
