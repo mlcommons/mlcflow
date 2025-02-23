a2964f6388d4cd415d3e1b76413d5b800929dec2
Arjun Suresh <arjun@gateoverflow.com>
2025-02-23 19:52:33 +0000

    Update main.py

--- mlc/main.py
+++ mlc/main.py
@@ -79,28 +79,6 @@ class Item:
         elif os.path.exists(json_file):
             utils.save_json(json_file, self.meta)
 
-
-class Repo:
-    def __init__(self, path, meta=None):
-        self.path = path
-        if meta:
-            self.meta = meta
-        else:
-            self._load_meta()
-        
-    
-    def _load_meta(self):
-        yaml_file = os.path.join(self.path, "meta.yaml")
-
-        json_file = os.path.join(self.path, "meta.json")
-
-        if os.path.exists(yaml_file):
-            self.repo_meta = utils.read_yaml(yaml_file)
-        elif os.path.exists(json_file):
-            self.repo_meta = utils.read_json(json_file)
-        else:
-            logger.info(f"No meta file found in {self.path}")
-
 class Automation:
     action_object = None
     automation_type = None
